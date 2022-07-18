d2aba151cf82d7c1fa673a594ca07f22151cee10
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-07-18 10:52:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -97,6 +97,19 @@ class BinarySearchTree{
         return data
     }
     
+    DFSInOrder() {
+    let data = [] 
+
+    function traverse(node) {
+        if(node.left) traverse(node.left)
+        data.push(node.val) 
+        if(node.right) traverse(node.right)
+    
+    }
+    traverse(this.root)
+    return data
+}
+    
 }
 
 let tree = new BinarySearchTree() 
